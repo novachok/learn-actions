b2c31cdf61f6c28728e61e141359853ad70f0050
Oleksandr Novachok <oleksandr.novachok@hopin.to>
2021-05-26 16:26:18 +0300

    add sqs client

--- app/src/destination/app.py
+++ app/src/destination/app.py
@@ -1,8 +1,18 @@
 import json
+import os
+import boto3
+
 
 def handler(event, context):
+    msg = {
+        'data': 'hello world'
+    }
+    client = boto3.client('sqs')
+    client.send_message(
+        QueueUrl=os.environ.get('QUEUE_URL'),
+        MessageBody=json.dumps(msg)
+    )
     return json.dumps({
         'statusCode': 200,
-        'body': '{"hello": "world"}'
+        'body': json.dumps(msg)
     })
-    
