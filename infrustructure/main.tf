f0ca2e30d1d21a91297452e4ad50d8c721a4ddb0
Oleksandr Novachok <oleksandr.novachok@hopin.to>
2021-05-27 08:43:15 +0300

    add lambda event sourcing

--- infrustructure/main.tf
+++ infrustructure/main.tf
@@ -103,3 +103,10 @@ resource "aws_lambda_function" "output_lambda" {
   timeout       = var.lambda_timeout
   memory_size   = var.lambda_memory_limit
 }
+
+resource "aws_lambda_event_source_mapping" "sqs_to_lambda" {
+  event_source_arn = aws_sqs_queue.events_queue.arn
+  enabled          = true
+  function_name    = aws_lambda_function.output_lambda.arn
+  batch_size       = 1
+}
