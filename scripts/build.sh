6d4c910fe71f51ba04faa8e0875f504eabb21911
Oleksandr Novachok <oleksandr.novachok@hopin.to>
2021-05-27 15:52:59 +0300

    fixes

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,10 +5,10 @@ TMP_DIR=$(mktemp -d -t ci-XXXX)
 
 cp $C_DIR/app/src/input/*py $TMP_DIR
 cd $TMP_DIR
-zip $C_DIR/build/input.zip ./*
+zip -r $C_DIR/build/input.zip ./*
 
 rm -rf $TMP_DIR/*
-cp $C_DIR/app/src/output/*py $TMP_DIR
-cp -R $C_DIR/app/src/output/lib $TMP_DIR
+cp -R $C_DIR/app/src/output/. $TMP_DIR/
+rm -rf $TMP_DIR/lib/__pycache__
 cd $TMP_DIR
-zip $C_DIR/build/output.zip ./*
+zip -r $C_DIR/build/output.zip ./*
